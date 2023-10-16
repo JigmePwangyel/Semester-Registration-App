165500b6e3794a888bc75f410a228da6021ad904
Jigme Phuntsho Wangyel <jphuntsho15@gmail.com>
2023-10-16 23:16:03 +0600

    Made Home Tab Scrollable

--- lib/pages/HomeTab.dart
+++ lib/pages/HomeTab.dart
@@ -11,27 +11,29 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Padding(
-        padding: EdgeInsets.only(left: 15, right: 15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 25),
-            Text(
-              "Welcome back Jigme",
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 23,
-                fontWeight: FontWeight.w500,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(left: 15, right: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 25),
+              Text(
+                "Welcome back Jigme",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
-            SizedBox(height: 25),
-            notRegisteredContainer(),
-            SizedBox(height: 25),
-            infoCard(),
-            SizedBox(height: 25),
-            homePageCard(),
-          ],
+              SizedBox(height: 25),
+              notRegisteredContainer(),
+              SizedBox(height: 25),
+              infoCard(),
+              SizedBox(height: 25),
+              homePageCard(),
+            ],
+          ),
         ),
       ),
     );
